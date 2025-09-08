72cebeb1534d14dae47560fa90b1cc9de16ed64a
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2025-09-08 19:14:19 +0300

    feature : Retested renderComponent...

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ import renderComponent from "./src/Component.js"
 // Have the body
 const body = document.body
 
+const Card = {
+    tag : "div",
+    props : {
+        textContent: " Hello I am here.",
+        className: "card",
+    }
+}
+
 
 // Mount when DOM is ready
 document.addEventListener("DOMContentLoaded", () => {
-
+    body.appendChild(renderComponent(Card))
 })
